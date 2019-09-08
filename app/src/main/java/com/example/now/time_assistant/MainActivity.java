e944b145ecf4288771c56bcb168de6a9fc882a73
Hyunjae Kim <peterhyunjae@naver.com>
2019-09-08 23:12:25 +0900

    방 만들때 안만들고 다시 돌아오면 튕기는 버그 수정

--- app/src/main/java/com/example/now/time_assistant/MainActivity.java
+++ app/src/main/java/com/example/now/time_assistant/MainActivity.java
@@ -97,51 +97,53 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1000) {
-            final String firstday = data.getExtras().getString("FirstDay");
-            final String lastday = data.getExtras().getString("LastDay");
-            Toast.makeText(MainActivity.this, "Day:" + firstday + " and " + lastday, Toast.LENGTH_SHORT).show();
+        if(resultCode == RESULT_OK) {
+            if (requestCode == 1000) {
+                final String firstday = data.getExtras().getString("FirstDay");
+                final String lastday = data.getExtras().getString("LastDay");
+                Toast.makeText(MainActivity.this, "Day:" + firstday + " and " + lastday, Toast.LENGTH_SHORT).show();
 
 
-            AppointmentData appointData = new AppointmentData();
-            appointData.setAppointment_date_created("2019/08/22");
-            appointData.setAppointment_img("for test");
-            appointData.setAppointment_name("약속 "+ list_apointment_index);
-            list_apointment_index++;
-            appointData.setAppointment_time("00:00~15:00");
-            appointData.setDate_start_end(firstday + "," + lastday);
+                AppointmentData appointData = new AppointmentData();
+                appointData.setAppointment_date_created("2019/08/22");
+                appointData.setAppointment_img("for test");
+                appointData.setAppointment_name("약속 " + list_apointment_index);
+                list_apointment_index++;
+                appointData.setAppointment_time("00:00~15:00");
+                appointData.setDate_start_end(firstday + "," + lastday);
 
-            list_apointment.add(appointData);
+                list_apointment.add(appointData);
 
-            mAdapter = new AppointmentAdapter(list_apointment, MainActivity.this, new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Object obj = v.getTag();
-                    if(obj != null){
-                        int position = (int)obj;
-                        ((AppointmentAdapter)mAdapter).getAppointment(position);
+                mAdapter = new AppointmentAdapter(list_apointment, MainActivity.this, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Object obj = v.getTag();
+                        if (obj != null) {
+                            int position = (int) obj;
+                            ((AppointmentAdapter) mAdapter).getAppointment(position);
 
 
-                        /**FirstDay & LastDay를 intent로 넘겨주어야함**/
-                        Intent intent = new Intent(MainActivity.this,Room.class);
+                            /**FirstDay & LastDay를 intent로 넘겨주어야함**/
+                            Intent intent = new Intent(MainActivity.this, Room.class);
 
-                        intent.putExtra("FirstDay", firstday);
-                        intent.putExtra("LastDay", lastday);
-                        intent.putExtra("FirstDay_Year", data.getExtras().getInt("FirstDay_Year"));
-                        intent.putExtra("FirstDay_Month", data.getExtras().getInt("FirstDay_Month"));
-                        intent.putExtra("FirstDay_Date", data.getExtras().getInt("FirstDay_Date"));
-                        intent.putExtra("LastDay_Year", data.getExtras().getInt("LastDay_Year"));
-                        intent.putExtra("LastDay_Month", data.getExtras().getInt("LastDay_Month"));
-                        intent.putExtra("LastDay_Date", data.getExtras().getInt("LastDay_Date"));
+                            intent.putExtra("FirstDay", firstday);
+                            intent.putExtra("LastDay", lastday);
+                            intent.putExtra("FirstDay_Year", data.getExtras().getInt("FirstDay_Year"));
+                            intent.putExtra("FirstDay_Month", data.getExtras().getInt("FirstDay_Month"));
+                            intent.putExtra("FirstDay_Date", data.getExtras().getInt("FirstDay_Date"));
+                            intent.putExtra("LastDay_Year", data.getExtras().getInt("LastDay_Year"));
+                            intent.putExtra("LastDay_Month", data.getExtras().getInt("LastDay_Month"));
+                            intent.putExtra("LastDay_Date", data.getExtras().getInt("LastDay_Date"));
 
-                        // intent.setPackage("com.android.chrome");   // 브라우저가 여러개 인 경우 콕 찍어서 크롬을 지정할 경우
-                        startActivity(intent);
-                    }else{          }
-                }
-            });
-            recyclerView.setAdapter(mAdapter);
+                            // intent.setPackage("com.android.chrome");   // 브라우저가 여러개 인 경우 콕 찍어서 크롬을 지정할 경우
+                            startActivity(intent);
+                        } else {
+                        }
+                    }
+                });
+                recyclerView.setAdapter(mAdapter);
+            }
         }
-
         if(requestCode == 2000){
             loadDatabase();
         }
